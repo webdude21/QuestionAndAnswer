77d81f189082dfe223c406aeac1e2e4ca2e1146e
webdude <webdude@webdude.eu>
2015-09-22 10:35:02 +0300

    made paging param optional

--- src/main/questionanswer/controller/Question.java
+++ src/main/questionanswer/controller/Question.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import questionanswer.service.QuestionService;
 
@@ -16,18 +16,12 @@ public class Question {
 	@Autowired
 	private QuestionService questionService;
 
-	@RequestMapping(value = "/question/{pageNumber}", method = RequestMethod.GET)
-	public String index(@PathVariable Integer pageNumber, ModelMap map) {
+	@RequestMapping(value = "/questions", method = RequestMethod.GET)
+	public String index(@RequestParam(value = "page", required = false, defaultValue = "1") int pageNumber,
+			ModelMap model) {
+		
 		Page<questionanswer.model.Question> page = questionService.getQuestions(pageNumber);
-	    int current = page.getNumber() + 1;
-	    int begin = Math.max(1, current - 5);
-	    int end = Math.min(begin + 10, page.getTotalPages());
-
-	    map.put("beginIndex", begin);
-	    map.put("endIndex", end);
-	    map.put("questionsList", page.getContent());
-	    map.put("currentIndex", current);
-			
+		model.put("questionsList", page.getContent());
 		return "Question/index";
 	}
 }
