aac98fe69b6f22c2ed5005497300067bc63d5cad
webdude <webdude@webdude.eu>
2015-10-07 01:13:39 +0300

    sorting works on the client

--- src/main/webapp/resources/scripts/controllers/questions-controller.js
+++ src/main/webapp/resources/scripts/controllers/questions-controller.js
@@ -1,16 +1,20 @@
 questionAndAnswer.controller('QuestionsController', function QuestionsController ($scope,
         QuestionResource){
-	
+
+	// initial values for the request
 	$scope.request = {
-	    sort: 'title', page: 0, size: 5, sortDirection: "asc"
+	    sort: 'title', page: 1, size: 5, sortDirection: 'asc'
 	};
 
-	$scope.query = function (queryObject){
-        if (queryObject.page < 0){
-            queryObject.page = 0;
-            return;
-        }
-		$scope.viewModel = QuestionResource.query($scope.request);
+	$scope.query = function (req){
+		if (req.page < 1) {
+			req.page = 1;
+			return;
+		}
+
+		$scope.viewModel = QuestionResource.query({
+		    sort: [req.sort, req.sortDirection].join(','), page: req.page - 1, size: req.size
+		});
 	};
 
 	$scope.query($scope.request);
--- src/main/webapp/resources/scripts/services/question/question-resource.js
+++ src/main/webapp/resources/scripts/services/question/question-resource.js
@@ -1,21 +1,20 @@
-questionAndAnswer.factory('QuestionResource', function QuestionResource($resource) {
-    return $resource('/api/questions/:id', null, {
-        getById: {
-            method: 'GET',
-            params: {id: '@id'},
-        },
-        update:{
-            method: 'PUT',
-            params: {id: '@id'}
-        },
-        deleteById:{
-            method: 'DELETE',
-            params: {id: '@id'}
-        },
-        getAll: {
-            method: 'GET',
-        },
-         query: {
+questionAndAnswer.factory('QuestionResource', function QuestionResource ($resource){
+	return $resource('/api/questions/:id', null, {
+	    getById: {
+	        method: 'GET', params: {
+		        id: '@id'
+	        },
+	    }, update: {
+	        method: 'PUT', params: {
+		        id: '@id'
+	        }
+	    }, deleteById: {
+	        method: 'DELETE', params: {
+		        id: '@id'
+	        }
+	    }, getAll: {
+		    method: 'GET',
+	    }, query: {
 	        method: 'GET', isArray: false
 	    }
 	});
