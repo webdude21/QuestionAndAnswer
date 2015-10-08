5cccca53c754284c1b3d1d1542b9c185e80ca1e3
webdude <webdude@webdude.eu>
2015-10-09 00:23:18 +0300

    extracted GlobalConstants

---
+++ src/main/questionanswer/GlobalConstants.java
@@ -0,0 +1,8 @@
+package questionanswer;
+
+public class GlobalConstants {
+
+	public static final String API_BASE_ROUTE = "/api";
+	
+	public static final String ADMIN = "ADMIN"; 
+}
--- src/main/questionanswer/config/Security.java
+++ src/main/questionanswer/config/Security.java
@@ -7,19 +7,24 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import static questionanswer.GlobalConstants.*;
+
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Security extends WebSecurityConfigurerAdapter {
 
+	private static final String QUESTIONS_ROUTE = API_BASE_ROUTE + "/questions/**"; 
+	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("webdude").password("webdude").roles("USER", "ADMIN");
+		auth.inMemoryAuthentication().withUser("webdude").password("webdude").roles("USER", ADMIN);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.POST, "/question").hasRole("ADMIN")
-				.antMatchers(HttpMethod.PUT, "/question/**").hasRole("ADMIN")
-				.antMatchers(HttpMethod.PATCH, "/question/**").hasRole("ADMIN").and().csrf().disable();
+		http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.POST, QUESTIONS_ROUTE).hasRole(ADMIN)
+				.antMatchers(HttpMethod.GET, QUESTIONS_ROUTE).hasRole(ADMIN)
+				.antMatchers(HttpMethod.PUT, QUESTIONS_ROUTE).hasRole(ADMIN)
+				.antMatchers(HttpMethod.PATCH, QUESTIONS_ROUTE).hasRole(ADMIN).and().csrf().disable();
 	}
 }
