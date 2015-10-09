cb66bf938f84cf5fe6927d8ae1b1d0e832575f5c
webdude <webdude@webdude.eu>
2015-10-09 20:43:12 +0300

    Added roles and rotues

---
+++ src/main/questionanswer/config/Roles.java
@@ -0,0 +1,10 @@
+package questionanswer.config;
+
+public class Roles {
+	
+	public static final String ADMIN = "ADMIN";
+	
+	public static final String USER = "USER";
+	
+	public static final String MODERATOR = "MODERATOR";
+}
