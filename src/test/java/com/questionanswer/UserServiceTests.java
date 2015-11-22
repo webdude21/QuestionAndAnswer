fc9050ccd8360826ba044ec5a44816492bc67f89
webdude21 <dimo.petrof@gmail.com>
2015-11-22 21:32:55 +0200

    added more tests for the userService

--- src/test/java/com/questionanswer/UserServiceTests.java
+++ src/test/java/com/questionanswer/UserServiceTests.java
@@ -1,9 +1,8 @@
 package com.questionanswer;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +14,6 @@ import com.questionanswer.model.User;
 import com.questionanswer.service.UserService;
 import com.questionanswer.service.UserServiceImpl;
 
-
 public class UserServiceTests {
 
 	private static final String ENCODED_PASSWORD = "encodedPassword";
@@ -25,14 +23,14 @@ public class UserServiceTests {
 	private static final String VALID_EMAIL = "webdude@dude.eu";
 
 	private User mockedUser;
-	
+
 	private UserRepository mockedUserRepo;
-	
+
 	private UserService userService;
-	
+
 	private PasswordEncoder mockedPasswordEncoder;
-	
-	public UserServiceTests(){
+
+	public UserServiceTests() {
 		this.mockedUser = new User("dimo", "petrov", VALID_EMAIL, RAW_PASSWORD);
 		this.mockedUserRepo = mock(UserRepository.class);
 		this.mockedPasswordEncoder = mock(PasswordEncoder.class);
@@ -41,21 +39,31 @@ public class UserServiceTests {
 
 	@Test
 	public void fetchingAnExistingUser() {
-		when(mockedUserRepo.findOneByEmail(VALID_EMAIL)).thenReturn(this.mockedUser);
+		when(this.mockedUserRepo.findOneByEmail(VALID_EMAIL)).thenReturn(this.mockedUser);
 		UserDetails user = this.userService.loadUserByUsername(VALID_EMAIL);
 		assertThat(user, equalTo(this.mockedUser));
+		verify(this.mockedUserRepo, times(1)).findOneByEmail(VALID_EMAIL);
 	}
-	
-	@Test(expected=UsernameNotFoundException.class)
+
+	@Test(expected = UsernameNotFoundException.class)
 	public void requiestingNonExistingUserThrowsException() {
 		when(mockedUserRepo.findOneByEmail(VALID_EMAIL)).thenReturn(null);
 		this.userService.loadUserByUsername(VALID_EMAIL);
 	}
-	
+
 	@Test
 	public void savingAnUserEncodesThePassword() {
 		when(mockedPasswordEncoder.encode(RAW_PASSWORD)).thenReturn(ENCODED_PASSWORD);
 		this.userService.save(this.mockedUser);
 		assertThat(this.mockedUser.getPassword(), equalTo(ENCODED_PASSWORD));
+		verify(this.mockedPasswordEncoder, times(1)).encode(RAW_PASSWORD);
+	}
+
+	@Test
+	public void callingSaveActuallySavesTheUserAndReturnsIt() {
+		when(mockedUserRepo.save(this.mockedUser)).thenReturn(this.mockedUser);
+		User user = this.userService.save(this.mockedUser);
+		assertThat(this.mockedUser, equalTo(user));
+		verify(this.mockedUserRepo, times(1)).save(this.mockedUser);
 	}
 }
