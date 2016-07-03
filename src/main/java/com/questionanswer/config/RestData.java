305832870a6a174ec8d1b8f3ab121fdbbff295a9
webdude21 <dimo.petrof@gmail.com>
2016-07-03 23:23:58 +0300

    added cors support

--- src/main/java/com/questionanswer/config/RestData.java
+++ src/main/java/com/questionanswer/config/RestData.java
@@ -1,9 +1,14 @@
 package com.questionanswer.config;
 
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.resource.GzipResourceResolver;
@@ -12,23 +17,38 @@ import org.springframework.web.servlet.resource.GzipResourceResolver;
 @Import(RepositoryRestMvcConfiguration.class)
 public class RestData extends RepositoryRestMvcConfiguration {
 
-	private static final int CACHE_TIME = 60 * 60 * 24; // 24 hours
-	protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		config.setBasePath(Routes.API_BASE_ROUTE);
-	}
+    private static final int CACHE_TIME = 60 * 60 * 24; // 24 hours
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		super.addResourceHandlers(registry);
-		registry.addResourceHandler("/**")
-			.addResourceLocations("classpath:/static/")
-			.setCachePeriod(CACHE_TIME)
-			.resourceChain(true)
-			.addResolver(new GzipResourceResolver());
-	}
+    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.setBasePath(Routes.API_BASE_ROUTE);
+    }
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(CACHE_TIME)
+                .resourceChain(true)
+                .addResolver(new GzipResourceResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
